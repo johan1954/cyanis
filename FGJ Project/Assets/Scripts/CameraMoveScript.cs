ffba8b103981ec955bcc0e4208904b1651ef608a
apezkin <ape.zkin@gmail.com>
2020-01-31 23:37:33 +0200

    movement, animations

---
+++ FGJ Project/Assets/Scripts/CameraMoveScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMoveScript : MonoBehaviour
+{
+    private Camera mainCamera;
+    private Camera secondCamera;
+    public Vector3 temp;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+        temp = new Vector3(23.5f, 0, 0);
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (this.name == "DoorTriggerRight") {
+            Debug.Log("Gameobject collided with " + other.name);
+            other.transform.position += (new Vector3(10, 0, 0));
+            Camera.main.transform.position += temp;
+            
+        }
+        if (this.name == "DoorTriggerLeft") {
+            Debug.Log("Gameobject collided with " + other.name);
+            other.transform.position -= (new Vector3(10, 0, 0));
+            Camera.main.transform.position -= temp;
+            this.transform.position -= temp;
+        }
+
+
+
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- FGJ Project/Assets/Scripts/Movement.cs
+++ FGJ Project/Assets/Scripts/Movement.cs
@@ -4,20 +4,51 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    public Quaternion originalRotation;
+    float rotationSpeed = 1.0f;
+    public float runSpeed = 5f;
+    public float jumpSpeed = 5f;
+    private Animator animator;
+    private Rigidbody2D rb;
+
+    //Save original rotation
+    void Start() {
+        originalRotation = transform.rotation;
+        animator = this.GetComponent<Animator>();
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
     // Update is called once per frame
     void Update()
     {
-        //Move the character
-        transform.Translate(Input.GetAxis("Horizontal") * 15f * Time.deltaTime, 0f, 0f);
+        var vertical = Input.GetAxis("Vertical");
+        var horizontal = Input.GetAxis("Horizontal");
+        var xVelocity = rb.velocity.x;
+        //Move the character 
+        transform.Translate(horizontal * runSpeed * Time.deltaTime, 0f, 0f);
+        transform.Translate(0f, vertical * 1f * Time.deltaTime, 0f);
 
         //Flip the character
         Vector3 characterScale = transform.localScale;  
-        if (Input.GetAxis("Horizontal") < 0) {
+        if (horizontal < 0) {
             characterScale.x = -1;
-        }  
-        if (Input.GetAxis("Horizontal") > 0) {
+            animator.SetInteger("walking", 1);
+        } else if (horizontal > 0) {
             characterScale.x = 1;
+            animator.SetInteger("walking", 1);
+        } else if (xVelocity != 0) {
+            animator.SetInteger("walking", 2);
+        } else {
+            animator.SetInteger("walking", 0);
         }
         transform.localScale = characterScale;
+
+        //Reset rotation
+        if (Input.GetKey("space")) {
+            transform.rotation = Quaternion.Slerp(transform.rotation, originalRotation, Time.time * rotationSpeed);
+            transform.Translate(0f, jumpSpeed * Time.deltaTime, 0f);
+            animator.SetInteger("walking", 2);
+        }
+
     }
 }
